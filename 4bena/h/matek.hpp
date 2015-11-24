e815ae874d2cdd478e78b42e60c336aa1f7fea3d
gyuri - sandbox-dev <szikoragy@vjsz.hu>
2015-11-24 15:23:47 +0100

    4bena

--- 4bena/h/matek.hpp
+++ 4bena/h/matek.hpp
@@ -38,9 +38,12 @@ double k_teglalap (double a, double b) {
 }
 
 // háromszög területe
+double t_haromszog(double a, double m) {
+    return (a*m)/2;
+}
 double t_haromszog(double a, double b, double c) {
-	double s = (a+b+c)/2;
-	return sqrt(s*(s-a)*(s-b)*(s-c));
+    double s = (a+b+c)/2;
+    return sqrt(s*(s-a)*(s-b)*(s-c));
 }
 
 // háromszög kerülete
@@ -94,6 +97,30 @@ double t_rombusz (double e, double f, int v=1) {
 // rombusz kerülete
 double k_rombusz (double a) {
 	return 4*a;
+}
+
+// másodfokú egyenlet megoldása
+double * masodfoku(double a, double b, double c) {
+    double res[3];
+    res[0], res[1], res[2] = 0;
+    double d = b*b-4*a*c;
+    if (d < 0) {
+        res[0] = 0;
+        return res;
+    };
+    
+    if (d == 0) {
+        res[0] = 1;
+        res[1] = -b/(2*a);
+        return res;
+    }
+    
+    if (d > 0) {
+        res[0] = 2;
+        res[1] = (-b)*(-sqrt(d))/(2*a);
+        res[2] = (-b)*(sqrt(d))/(2*a);
+        return res;
+    }
 }
 
 #endif
--- 4bena/matek.cpp
+++ 4bena/matek.cpp
@@ -11,6 +11,18 @@ int main () {
 	cout << "Kör területe r=4:\t"<< t_kor(4) << endl;
 	cout << "Háromszög területe (3,4,5):\t" << t_haromszog(3,4,5) << endl;
 	cout << "Rombusz e=3, f=5:\t" << t_rombusz(3,5) << endl;
-	cout << "Rombusz e=3, f=5:\t" << t_rombusz(3,5,0) << endl;
+	cout << "Rombusz a=3, m=5:\t" << t_rombusz(3,5,0) << endl;
+    cout << "Háromszög területe (4,5):\t" << t_haromszog(4,5) << endl;
+    cout << "Háromszög területe (3,4,5):\t" << t_haromszog(3,4,5) << endl;
+
+
+// másodfokú megoldáshoz
+    double *r;
+    
+    r = masodfoku(-1,5,-4);
+    
+    cout << *r << endl;
+    cout << *(r+1) << endl; 
+    cout << *(r+2) << endl;
 	return 0;
 }
