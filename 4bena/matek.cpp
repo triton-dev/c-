ca939a61556886c04ea310a59ba09b93580f15e6
gyuri - sandbox-dev <szikoragy@vjsz.hu>
2015-11-25 08:31:37 +0100

    4bena mutatózás

--- 4bena/matek.cpp
+++ 4bena/matek.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 int main () {
 
-	cout << "Kör területe r=4:\t"<< t_kor(4) << endl;
+	cout << "Kör területe r=5:\t"<< t_kor(5) << endl;
 	cout << "Háromszög területe (3,4,5):\t" << t_haromszog(3,4,5) << endl;
 	cout << "Rombusz e=3, f=5:\t" << t_rombusz(3,5) << endl;
 	cout << "Rombusz a=3, m=5:\t" << t_rombusz(3,5,0) << endl;
@@ -31,8 +31,21 @@ int main () {
 	
 	masodfoku(a,b,c);
     
-   
-   
+	cout << "\n********************************************\n";
+
+	double d[5] = {0,1,2,3,4};
+	double *p[5];
+
+	for(int i= 0; i<5; i++) {
+		p[i] = &d[i];
+		cout << &d[i] << " : " << d[i] << "->"  << *p[i] << endl;
+	}
+	cout << "---------------------------------------------\n";
+	cout << p+0 << "\t" << *(p+0) << "\t" << *p[0] << endl;   
+	cout << p+1 << "\t" << *(p+1) << "\t" << *p[1] << endl;   
+	cout << p+2 << "\t" << *(p+2) << "\t" << *p[2] << endl;   
+	cout << p+3 << "\t" << *(p+3) << "\t" << *p[3] << endl;   
+	cout << p+4 << "\t" << *(p+4) << "\t" << *p[4] << endl;   
    
    
 // main() visszatérés   
