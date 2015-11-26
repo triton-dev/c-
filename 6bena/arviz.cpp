4422e1375f27477cff8319db8a886e808ca40fb8
gyuri - sandbox-dev <szikoragy@vjsz.hu>
2015-11-26 11:41:53 +0100

    6bena arviz

--- 6bena/arviz.cpp
+++ 6bena/arviz.cpp
@@ -9,10 +9,26 @@ const int MIN_ERTEK = 0;
 const int MAX_ERTEK = 3000;
 
 const int FOK_1 = 800;
+const int FOK_2 = 900;
 
 int darab = 0;
 
 int ertek[MAX_MERES];
+
+int arviz = 0;
+bool nem_szakasz = true;
+
+int szakasz[MAX_MERES];
+int szakaszszamlalo = 1;    
+    
+struct av {
+    int eleje;
+    int vege;
+    int mennyi;
+};   
+
+av avsz[MAX_MERES]; 
+int avsz_ct = 1;
 
 void beolvas_darab() {
 	cout << "*****************" << endl;
@@ -67,6 +83,48 @@ void beolvas_meres(int db) {
 	}
 }
 
+
+void kiir_szakasz() {
+    for (int i=1; i<=darab; i++) {
+        if(ertek[i] > FOK_1 && nem_szakasz) {
+            arviz++;
+            nem_szakasz = false;
+        }
+        if(ertek[i] <= FOK_1) {
+            nem_szakasz = true;
+        }
+    }
+    cout << "Szakaszok száma: " << arviz << endl;
+}
+
+void kiir_szakaszok() {
+    nem_szakasz = true;
+    for (int i=1; i<=darab; i++) {
+        if(ertek[i] > FOK_1 && nem_szakasz) {
+            arviz++;
+            nem_szakasz = false;
+            cout << i << "-";
+            szakasz[szakaszszamlalo] = i;
+            szakaszszamlalo++;
+        }
+        if(ertek[i] <= FOK_1) {
+            nem_szakasz = true;
+            cout << i-1 << " ";
+            szakasz[szakaszszamlalo] = i-1;
+            szakaszszamlalo++;
+        }
+    }
+    if (ertek[darab] > FOK_1) {
+        cout << darab;
+        szakasz[szakaszszamlalo] = darab;
+        szakaszszamlalo++;
+        
+    }
+    cout << endl;
+
+}
+
+
 int main() {
 	
 	beolvas_darab();
@@ -74,21 +132,50 @@ int main() {
 	beolvas_meres(darab);
 	
 	felsorol_meres();
+    
+    kiir_szakasz();
 	
-	int arviz = 0;
-	bool nem_szakasz = true;
+    kiir_szakaszok();
+    
+    if(arviz ==0) {
+        cout << endl << "************************" << endl;
+        cout <<          "*****     VÉGE    *****" << endl;
+        return 0;
+    }
 	
-	for (int i=1; i<=darab; i++) {
-		if(ertek[i] > FOK_1 && nem_szakasz) {
-			arviz++;
-			nem_szakasz = false;
-		}
-		if(ertek[i] <= FOK_1) {
-			nem_szakasz = true;
-		}
-	}
-	cout << "Szakaszok száma: " << arviz << endl;
+
+    
 	
+    for(int i=1; i<szakaszszamlalo; i = i+2) {
+        int as=0;
+        avsz[avsz_ct].eleje = szakasz[i]; 
+        avsz[avsz_ct].vege = szakasz[i+1];
+        for(int o=szakasz[i]; o<=szakasz[i+1]; o++) {
+            
+            if (ertek[o] > FOK_1 && ertek[o] < FOK_2) {
+                as++;
+            }
+        }
+        cout << as << " ";
+        avsz[avsz_ct].mennyi = as;
+        avsz_ct++;
+    }
+    cout << endl;
+	
+    for(int i= 1; i< avsz_ct; i++) {
+        cout << avsz[avsz_ct].eleje << "-";
+        cout << avsz[avsz_ct].vege <<  ":";
+        cout << avsz[avsz_ct].mennyi << endl;
+    }
+    
+    cout << "Strukt próba..." << endl;
+    for (int i=1; i<MAX_MERES; i++) {
+        avsz[i].eleje = i;
+        avsz[i].vege = i+MAX_MERES;
+        avsz[i].mennyi = i*MAX_MERES;
+        cout << avsz[i].eleje << "\t" << avsz[i].vege << "\t" << avsz[i].mennyi << endl;
+    }    
+    
 	
 	
 	
