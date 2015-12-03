5a90b5fc1df8667af63117cfb094b20de38b1cef
gyuri <szikoragy@gmail.com>
2015-12-03 22:15:24 +0100

    Merge-re előkészítve, vector a dolgozókhoz

--- tel/telcsi/main.cpp
+++ tel/telcsi/main.cpp
@@ -1,13 +1,18 @@
-#include<string>
-#include<iostream>
-
+#include <string>
+#include <iostream>
+#include <vector>
 
 #include "Dolgozo.h"
 
 using namespace std;
 
+vector<Dolgozo> dolg;
+
 int main()
 {
+
+	//dolg.resize(10);
+
 
     Dolgozo d = Dolgozo ("Gőz Gizi","gizike");
     d.print();
@@ -32,6 +37,30 @@ int main()
     d.swapStatusz();
     d.print();
 
+	Dolgozo c = Dolgozo("Makk Marci","mikkamakka");
+	Dolgozo b = Dolgozo("Pityke Panni","pankamanka");
+
+	dolg.push_back(d);
+	dolg.push_back(c);
+	dolg.push_back(b);
+    cout << "Dolgozo vektor mérete: " << dolg.size() << endl;
+    cout << "Dolgozo vektor kapacitása: " << dolg.capacity() << endl;
+    Dolgozo x = dolg[0];
+    cout << x.getTeljesNev() << endl;
+    x = dolg[1];
+    cout << x.getTeljesNev() << endl;
+	x = dolg.at(2);
+	cout << x.getTeljesNev() << endl;
+	dolg.at(2).setTeljesNev("Jankó Mankó");
+	cout << dolg.at(2).getTeljesNev() << endl;
+
+	for(int i=0; i < dolg.size(); i++) {
+		dolg.at(i).print();
+	}
+
+
+
+
 //main vége
-    return 0;
+    return EXIT_SUCCESS;
 }
